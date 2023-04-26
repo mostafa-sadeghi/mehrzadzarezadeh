b6535d8ef45ef28306373389b84ab66466802bcd
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-26 20:57:52 +0330

    sadeghi

---
+++ s1.py
@@ -0,0 +1,66 @@
+# a = 4
+# b = 5
+
+# if a > b:
+#     print(">")
+#     print("something else")
+
+# print("outside")
+
+# <   <=    >    >=     ==    !=
+
+
+# string1 = "c"
+# string2 = "b"
+
+# if string1 > string2:
+#     print("string1 is greater than string2")
+
+# name1 = "amir"
+# name2 = "amin"
+# if name1 > name2:
+#     print(f"{name1} is greater than {name2}")
+
+# number1 = int(input("enter a number: "))
+# number2 = int(input("enter a number: "))
+
+# if number1 == number2:
+#     print(f"{number1} == {number2}")
+
+# elif number1 > number2:
+#     print(f"{number1} > {number2}")
+# else:
+#     print(f"{number1} < {number2}")
+
+
+# a = 3
+# b = 5
+# c = 2
+
+# if a > b and a > c:
+#     print("a is the greatest")
+
+# if a > b or a > c:
+#     print("a is greater than b or c")
+
+# x = True
+# print(type(x))
+
+# or     and      not
+
+# print(not(x))
+# print(not(False))
+
+# a = b = 3
+
+# print(a, b)
+
+# c = a == b
+# print(c)
+
+
+user_name = input("What's your name? ").lower()
+if user_name == "artin" or user_name == "sara":
+    print("You have a nice name.")
+else:
+    print("your name is ok.")
