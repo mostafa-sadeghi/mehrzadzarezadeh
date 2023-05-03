f4f3c000ab7f9f4105515657c800d36351673109
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-03 20:34:14 +0330

    sadeghi

---
+++ s2.py
@@ -0,0 +1,48 @@
+# for i in range(5):
+#     print("Hello")
+#     print("welcome")
+# print("outside")
+
+# for i in range(5):
+#     print(i)
+
+# for n in range(1,11):
+#     print(n)
+
+# for number in range(2,12,2):
+#     print(number)
+
+# اعداد فرد را پرینت کنید
+
+# for x in range(10,0,-1):
+#     print(x)
+
+# for s in 'python':
+#     print(s.upper(),end=' ')
+
+# s = 0
+# for num in [2,6,3,8,9,11]:
+#     s += num # s = s + num
+# print(s)
+
+# exercise: از لیست بالا مجموع اعداد فرد را محاسبه و نمایش بدی
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+
+#     for j in range(3):
+#         print("b")
+
+# res = 0
+# for i in range(10_000_000,20_000_001):
+#     res += i
+# print(res)
+
+# محاسبه مجموع اعداد زوج بین ده میلیون و بیست میلیون
