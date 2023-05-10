6c69b9ffc611a1f567f7babc1a592595930158cf
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-10 20:57:47 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,26 @@
+
+
+# number = int(input('enter a number: '))
+
+# if number % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# برنام ای بنویس که نام یک دانش آموز را از ورودی دریافت نماید و 
+# نمرات سه درس او را بگیرد
+# معدلش را حساب کند
+# اسم و معدلش را پرینت نماید.
+
+
+
+result = 0
+
+done = True
+while done:
+    number = float(input('enter a number: '))
+    result += number
+    if input('Do you want to quit? (yes or no)> ') == 'yes':
+        break
+        # done = False
+print("result is:", f'{result:g}')
