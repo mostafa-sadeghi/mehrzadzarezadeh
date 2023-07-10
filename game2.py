b0fb0d90f9323d36cce2bedbb07b9233bf603210
msa <msai.sadehi@gmail.com>
2023-07-10 21:59:33 +0330

    sadeghi

--- game2.py
+++ game2.py
@@ -1,3 +1,4 @@
+from random import randint
 import pygame
 
 pygame.init()
@@ -10,6 +11,11 @@ pygame.display.set_caption("game2")
 GREEN = (0, 255, 0)
 DARKGREEN = (10, 50, 10)
 BLACK = (0, 0, 0)
+
+
+PLAYER_VELOCITY = 5
+FPS = 60
+clock = pygame.time.Clock()
 
 system_font = pygame.font.SysFont('calibri', 64)
 my_font = pygame.font.Font("AttackGraffiti.ttf", 32)
@@ -40,6 +46,12 @@ player_rect.centerx = 50
 player_rect.bottom = WINDOW_HEIGHT
 
 
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.x = WINDOW_WIDTH + 200
+coin_rect.y = randint(64, WINDOW_HEIGHT - 32)
+
+
 bgsound = pygame.mixer.Sound("bgmusic.mp3")
 loss = pygame.mixer.Sound("loss.wav")
 success = pygame.mixer.Sound("success.wav")
@@ -48,10 +60,35 @@ bgsound.play(-1)
 
 running = True
 while running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
+        # if event.type == pygame.KEYDOWN:
+
+        #     if event.key == pygame.K_RIGHT:
+        #         player_rect.x += PLAYER_VELOCITY
+
+        #     if event.key == pygame.K_LEFT:
+        #         player_rect.x -= PLAYER_VELOCITY
+
+        #     if event.key == pygame.K_UP:
+        #         player_rect.y -= PLAYER_VELOCITY
+
+        #     if event.key == pygame.K_DOWN:
+        #         player_rect.y += PLAYER_VELOCITY
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP] and player_rect.top > 74:
+        player_rect.y -= PLAYER_VELOCITY
+
+    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
+        player_rect.y += PLAYER_VELOCITY
+
+    display_surface.fill(BLACK)
     display_surface.blit(dragon_left_image,
                          dragon_left_rect)
     display_surface.blit(dragon_right_image, dragon_right_rect)
@@ -64,5 +101,6 @@ while running:
     # display_surface.blit(system_text, system_text_rect)
     # display_surface.blit(my_text, my_text_rect)
     pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
