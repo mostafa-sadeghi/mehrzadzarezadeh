2b452eb8572adfe825d012d87f54579ece6f46d5
msa <msai.sadehi@gmail.com>
2023-07-26 20:10:58 +0330

    dragon game

--- dragon_game/main.py
+++ dragon_game/main.py
@@ -17,7 +17,6 @@ PLAYER_STARTING_VELOCITY = 5
 COIN_STARTING_VELOCITY = 8
 FPS = 60
 
-
 player_velocity = PLAYER_STARTING_VELOCITY
 player_lives = PLAYER_STARTING_LIVES
 coin_velocity = COIN_STARTING_VELOCITY
@@ -26,29 +25,40 @@ score = 0
 
 clock = pygame.time.Clock()
 
-
 my_font = pygame.font.Font("AttackGraffiti.ttf", 32)
 
-
-score_text = my_font.render(f"Score: {score}",\
-                             False, GREEN,\
-                                  DARKGREEN)
+score_text = my_font.render(f"Score: {score}", \
+                            False, GREEN, \
+                            DARKGREEN)
 score_rect = score_text.get_rect()
 score_rect.midleft = (90, 38)
 
 title_text = my_font.render("Dragon Game", True, DARKGREEN, GREEN)
 title_rect = title_text.get_rect()
-title_rect.centerx = WINDOW_WIDTH/2
+title_rect.centerx = WINDOW_WIDTH / 2
 title_rect.centery = 38
 
 lives_text = my_font.render(f"Lives: {player_lives}", True, GREEN, DARKGREEN)
 lives_rect = lives_text.get_rect()
 lives_rect.center = (WINDOW_WIDTH - 150, 38)
 
+
+game_over_text = my_font.render("Game Over", False, GREEN)
+game_over_text_rect = game_over_text.get_rect()
+game_over_text_rect.center = (WINDOW_WIDTH/2,\
+                              WINDOW_HEIGHT/2)
+
+
+continue_text = my_font.render("Press any key to continue...",\
+                               False, DARKGREEN)
+continue_text_rect = continue_text.get_rect()
+continue_text_rect.center = (WINDOW_WIDTH/2,\
+                             WINDOW_HEIGHT/2 + 50)
+
+
 dragon_left_image = pygame.image.load("dragon_left.png")
 dragon_left_rect = dragon_left_image.get_rect()
 dragon_left_rect.topleft = (0, 0)
-
 
 dragon_right_image = pygame.image.load("dragon_right.png")
 dragon_right_rect = dragon_right_image.get_rect()
@@ -60,12 +70,10 @@ player_rect = player.get_rect()
 player_rect.centerx = 50
 player_rect.bottom = WINDOW_HEIGHT
 
-
 coin_image = pygame.transform.scale(pygame.image.load("coin.png"), (32, 32))
 coin_rect = coin_image.get_rect()
 coin_rect.x = WINDOW_WIDTH + 200
 coin_rect.y = randint(64, WINDOW_HEIGHT - 32)
-
 
 bgsound = pygame.mixer.Sound("bgmusic.mp3")
 loss = pygame.mixer.Sound("loss.wav")
@@ -105,12 +113,37 @@ while running:
         coin_rect.x = WINDOW_WIDTH + 200
         coin_rect.y = randint(64, WINDOW_HEIGHT - 32)
 
-
-
-
-
     lives_text = my_font.render(f"Lives: {player_lives}", True, GREEN, DARKGREEN)
     score_text = my_font.render(f"Score: {score}", True, GREEN, DARKGREEN)
+
+    if player_lives == 0:
+        display_surface.blit(game_over_text,\
+                             game_over_text_rect)
+        display_surface.blit(continue_text,\
+                             continue_text_rect)
+        pygame.display.update()
+        bgsound.stop()
+
+
+
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    is_paused = False
+                    player_lives = PLAYER_STARTING_LIVES
+                    score = 0
+                    coin_velocity = COIN_STARTING_VELOCITY
+                    bgsound.play(-1)
+
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
+
+
+
+
     display_surface.fill(BLACK)
     display_surface.blit(dragon_left_image,
                          dragon_left_rect)
@@ -124,8 +157,6 @@ while running:
     display_surface.blit(score_text, score_rect)
     display_surface.blit(title_text, title_rect)
     display_surface.blit(lives_text, lives_rect)
-
-
 
     pygame.display.update()
     clock.tick(FPS)
