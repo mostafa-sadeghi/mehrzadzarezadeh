32cf88545514c047d476d5541c6eba2235b81eca
msa <msai.sadeghi@gmail.com>
2023-10-25 20:58:54 +0330

    s

---
+++ platformer/main.py
@@ -0,0 +1,36 @@
+import pygame
+from constants import *
+from world import World
+from levels.level1 import world_data
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bg = pygame.image.load("assets/backg.png")
+bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+bg_rect = bg.get_rect()
+bg_rect.topleft = (0, 0)
+
+
+game_world = World(world_data)
+
+
+def draw_grid():
+    for i in range(20):
+        pygame.draw.line(screen, (255, 255, 255),
+                         (i * TILE_SIZE, 0), (i*TILE_SIZE, SCREEN_HEIGHT))
+    for j in range(14):
+        pygame.draw.line(screen, (255, 255, 255),
+                         (0, j * TILE_SIZE), (SCREEN_WIDTH, j * TILE_SIZE))
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(bg, bg_rect)
+    draw_grid()
+    game_world.draw(screen)
+    pygame.display.update()
---
+++ platformer/world.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+
+class World:
+    def __init__(self, data):
+        self.tile_list = []
+        dirt_img = pygame.image.load("assets/dirt.png")
+
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                if data[i][j] == 1:
+                    img = pygame.transform.scale(
+                        dirt_img, (TILE_SIZE, TILE_SIZE))
+                    img_rect = img.get_rect()
+                    img_rect.x = j * TILE_SIZE
+                    img_rect.y = i * TILE_SIZE
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if 
+
+    def draw(self, screen):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
